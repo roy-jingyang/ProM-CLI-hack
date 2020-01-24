fb5894126d185058d7db05c11953ebd18ccde492
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2020-01-24 14:42:17 +1000

    restructure files

--- ProM69_CLI.sh
+++ ProM69_CLI.sh
@@ -5,8 +5,9 @@
 ###
 PROGRAM=ProM
 ROOTDIR=./prom6/prom69 # set this to the directory where ProM.ini is
-CP=./ProM69_dist/ProM-Framework-6.9.97.jar:./ProM69_dist/ProM-Contexts-6.9.56.jar:./ProM69_dist/ProM-Models-6.9.32.jar:./ProM69_dist/ProM-Plugins-6.9.67.jar
-LIBDIR=./ProM69_lib
+#CP=./ProM69_dist/ProM-Framework-6.9.97.jar:./ProM69_dist/ProM-Contexts-6.9.56.jar:./ProM69_dist/ProM-Models-6.9.32.jar:./ProM69_dist/ProM-Plugins-6.9.67.jar
+DISTDIR=${ROOTDIR}"/ProM69_dist"
+LIBDIR=${ROOTDIR}"/ProM69_lib"
 MAIN=org.processmining.contexts.cli.CLI
 
 ####
@@ -18,20 +19,24 @@ JAVA=java
 ## Main program
 ###
 
+CP=./
+
 add() {
 	CP=${CP}:$1
 }
 
+for jar in $DISTDIR/*.jar
+do
+    add $jar
+done
 
 for lib in $LIBDIR/*.jar
 do
 	add $lib
 done
 
-#set -x
 
-CP=${ROOTDIR}:${CP}
-LIBDIR=${ROOTDIR}:${LIBDIR}
+set -x
 
 $JAVA \
     -Djava.system.class.loader=org.processmining.framework.util.ProMClassLoader \
